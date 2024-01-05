f69bae87dec21afbfeab7f76b99a399ea1cbbb84
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2024-01-05 21:20:48 +0100

    exercise completed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str, count) {
+    // my simple solution
+    let newStr = "";
+    for (let i = 0; i < count; i++) {
+        newStr += str;
+    }
 
+    return newStr;
+    
+    // this is another solutio to this exercise
+    // let newStr = [];
+    // for (let i = 0; i < count; i++) {
+    //     newStr.push(str);
+    // }
+
+    // return newStr;
 };
 
 // Do not edit below this line
