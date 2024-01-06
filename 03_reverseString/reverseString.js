62fcce4128bfe5be76734a7abc7736f2310e555c
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2024-01-06 12:52:29 +0100

    Exercise completed.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,15 @@
-const reverseString = function(str) {
-    let reversedStr = str.split("").reverse().join("");
+const reverseString = function(word) {
+    return word.split('').reverse().join('');
 
-    return reversedStr;
+    // this is another solution - a complete waste of time :)
+    
+    // let reversedStr = '';
+    // let wordAsArr = word.split('');
+    // for (let i = word.length - 1; i >= 0; i--) {
+    //     reversedStr += wordAsArr[i];
+    // }
+
+    // return reversedStr;
 };
 
 // Do not edit below this line
