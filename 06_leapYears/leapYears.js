38c552d0bed35346cb8e6289e40d6773d662c74a
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2024-01-07 14:06:15 +0100

    Exercise completed.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // if (year % 100 === 0) {
+    //     if (year % 400 === 0) return true;
+    //     else return false;
+    // }
+    // else if (year % 4 === 0) return true;
+    // else return false;
 
+    return (year % 4 === 0) && ((year % 100 !== 0) || (year % 400 === 0));
 };
 
+// console.log(leapYears(1997));
 // Do not edit below this line
 module.exports = leapYears;
